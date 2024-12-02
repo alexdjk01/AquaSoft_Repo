3234d82d86bc1b4165a1c1d40c8725bb0e282729
Alexandru Mihai Ionel <ionelalexandru01@gmail.com>
2024-12-02 14:58:43 +0200

    commit2
    -Installed ts-node packages
    -Renamed HomeWork1.js to .ts

---
+++ HomeWork1.ts
@@ -0,0 +1,15 @@
+
+// 1.1. ES6 - Methods (examples, explanations).
+// 1.2. Difference between var, let, and const.
+// 1.3. Spread operator.
+// 1.4. Objects: How to iterate over an object, deep copy.
+// 1.5. Arrays - accessor, iteration, and mutator methods (which they are, how to use them).
+// 1.6. Promises. Callback.
+// 1.7. Async. Await.
+// 1.8. Closures.
+// 1.9. useState. useRef.
+
+// Solutions for the questions
+
+console.log("Starting internship practice ...");
+console.log("---------------- Task1 ----------------");
