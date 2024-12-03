d5f0564bcddcaf1cb9447d1963471ceffaa5aefb
Alexandru Mihai Ionel <ionelalexandru01@gmail.com>
2024-12-04 00:47:33 +0200

    commit10 - Sequelize.ts updated
    Changes package.json and tsconfig.json in order to be able to use imports and import sequelize.
    Created the first model class : Hotel

---
+++ Homework2/database/sequelize.ts
@@ -0,0 +1,18 @@
+import  {Sequelize}  from 'sequelize';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const DB_NAME:string = process.env.DB_NAME as string;
+const DB_USER:string = process.env.DB_USER as string;
+const DB_PASSWORD:string = process.env.DB_PASSWORD as string;
+const DB_HOST:string = process.env.DB_HOST as string;
+const DB_DIALECT = process.env.DB_DIALECT as "mysql" | "postgres" | "sqlite" | "mariadb";
+const DB_PORT:number = Number(process.env.DB_PORT);
+
+const sequelize = new Sequelize( DB_NAME,DB_USER,DB_PASSWORD , {
+  host: DB_HOST,
+  dialect:DB_DIALECT,
+  port:DB_PORT
+});
+
+export default sequelize;
--- Homework2/index.ts
+++ Homework2/index.ts
@@ -1,1 +1,12 @@
-console.log('hellow');
+import sequelize from "./database/sequelize.js";
+
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+startServer();
