026b20ac29fb80bd12d0ceca2ecbbca312231843
Alexandru Mihai Ionel <ionelalexandru01@gmail.com>
2024-12-18 10:29:16 +0200

    Commit 26 - Moving files

--- Homework4/homework2_nest/src/app/app.controller.spec.ts
+++ Homework4/homework2_nest/src/app/app.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from '../app/app.controller.js';
+import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 
 describe('AppController', () => {
